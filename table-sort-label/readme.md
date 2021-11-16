a2e918c1d0e0a512521d6ec96c4642d3c25e3e1f
trongtd <gacontuyenchien1@gmail.com>
2021-11-17 00:00:25 +0700

    feature: add table-sort-label

---
+++ table-sort-label/readme.md
@@ -0,0 +1,2 @@
+# README
+This is for add an icon to each header when click to sort by its column
---
+++ table-sort-label/table-sort-label.js
@@ -0,0 +1,69 @@
+const HEADERS = {
+    "Company": 0,
+    "Contact": 1,
+    "Country": 2
+}
+
+const aStatus = [0, 0, 0];
+const aTHeads = document.querySelectorAll("th");
+
+function refreshSortIcons(){
+    aTHeads.forEach( th =>{
+        let upEl = th.querySelector(".sort-up");
+        upEl.style.display = "none";
+        let downEl = th.querySelector(".sort-down");
+        downEl.style.display = "none";
+    });
+}
+
+/**
+ * 
+ * @param {"th" html element} thEl
+ * @param {int} status. -1: up, 1: down
+ */
+function showSortIcon( thEl, status ){
+    if ( status === -1 ){
+        let upEl = thEl.querySelector(".sort-up");
+        upEl.style.display = "block";
+    }else if ( status === 1){
+        let downEl = thEl.querySelector(".sort-down");
+        downEl.style.display = "block";
+    } else{
+        // Do nothing
+    }
+}
+
+function changeSortState( event ){
+    console.log("event: ", event.target);
+    let thEl = event.target;
+    let thTxt = thEl.innerText;
+    switch ( aStatus[HEADERS[thTxt]]){
+        case 0:
+        case 1: 
+            aStatus[HEADERS[thTxt]] = -1;
+            break;
+        case -1:
+            aStatus[HEADERS[thTxt]] = 1;
+            break;
+        default:
+            // Do nothing
+            break;
+    }
+
+    refreshSortIcons();
+    showSortIcon( thEl, aStatus[HEADERS[thTxt]]);
+}
+
+aTHeads.forEach( th =>{
+    th.style.position = "relative";
+    const sortDown = `<svg class="sort-down" style="display: none; transform: rotate(180deg); position: absolute; right: 0; bottom: 2px;" width="8" height="8" fill="currentColor"  viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M7.022 1.566a1.13 1.13 0 0 1 1.96 0l6.857 11.667c.457.778-.092 1.767-.98 1.767H1.144c-.889 0-1.437-.99-.98-1.767L7.022 1.566z"/>
+                        </svg>`;
+    th.innerHTML += sortDown;
+    const sortUp = `<svg class="sort-up" style="display: none; position: absolute; right: 0; bottom: 11px;" width="8" height="8" fill="currentColor" viewBox="0 0 16 16">
+    <path fill-rule="evenodd" d="M7.022 1.566a1.13 1.13 0 0 1 1.96 0l6.857 11.667c.457.778-.092 1.767-.98 1.767H1.144c-.889 0-1.437-.99-.98-1.767L7.022 1.566z"/>
+    </svg>`;
+    th.innerHTML += sortUp;
+    th.addEventListener("click", changeSortState);
+});
+console.log( aTHeads );
